8fbfcbe04d4c5608a53686afe3ba17083c8b13eb
Viktor <v.kostov@sap.com>
2026-03-25 09:16:03 +0200

    Use local Backupbucket and Backupentry

--- pkg/gardenadm/cmd/init/init.go
+++ pkg/gardenadm/cmd/init/init.go
@@ -507,7 +507,6 @@ func bootstrapControlPlane(ctx context.Context, opts *Options) (*botanist.Garden
 	if err != nil {
 		return nil, err
 	}
-	b.StoreContainer = opts.StoreContainer
 
 	if opts.Zone != "" {
 		b.Zone = ptr.To(opts.Zone)
