7885e131e1a949f4e35ceda4bbca04a7304feac0
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2026-06-24 10:46:57 +0300

    Remove the `--use-bootstrap-etcd` flag during restore in the disaster recovery scripts (#15)
    * `gardenadm discover`: Export BackupEntry and BackupBucket Secrets
    
    * Remove the `--use-bootstrap-etcd` flag during restore in the disaster recovery scripts
    
    * chore: Extract the etcd backup preparation to a separate section
    
    * Nit: Update step log in the scripts
    
    * Trigger etcd delta snapshot without port-forward
    
    The etcd-main Pod runs in the host network. After takeover from the etcd-druid, it gets the `backup-restore` sidecar container. The container is accessible from the `gind-machine-0` Node. There is no need to port-forward from the host machine (the macOs) in order to access it.

--- pkg/gardenadm/cmd/init/init.go
+++ pkg/gardenadm/cmd/init/init.go
@@ -100,8 +100,6 @@ func runRecover(ctx context.Context, opts *Options) error {
 		return fmt.Errorf("failed preparing second recovery phase: %w", err)
 	}
 
-	phaseOpts.UseBootstrapEtcd = true
-
 	return runInit(ctx, &phaseOpts)
 }
 
